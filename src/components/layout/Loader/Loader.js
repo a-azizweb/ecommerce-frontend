7b0359823f51726a69adbcddd81cb95f82056365
a-azizweb <azizahmadhaider@gmail.com>
2023-01-28 15:07:31 +0500

    changing loader

--- src/components/layout/Loader/Loader.js
+++ src/components/layout/Loader/Loader.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './Loader.css';
 const Loader = () => {
   return (
-    <div className="loading">
-      <div></div>
-    </div>
+    // <div className="loading">
+    //   <div></div>
+    // </div>
+    <div class="loading">
+  <span></span>
+  <span></span>
+  <span></span>
+  <span></span>
+</div>
   );
 };
 
