41e771d27d04a10f0d25f3139284078c2f9a6a54
a-azizweb <azizahmadhaider@gmail.com>
2023-01-28 16:05:29 +0500

    updating loader

--- src/components/layout/Loader/Loader.js
+++ src/components/layout/Loader/Loader.js
@@ -2,8 +2,18 @@ import React from 'react';
 import './Loader.css';
 const Loader = () => {
   return (
-    <div className="loading">
-      <div></div>
+    // <div className="loading">
+    //   <div></div>
+    // </div>
+
+    <div className="container loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <h1>Loading</h1>
     </div>
   );
 };
